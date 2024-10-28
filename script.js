4cd879b6d027e46617cc2a56ae92c06d841e4952
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2024-10-28 19:25:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.getElementById('submit').addEventListener('click', () => {
 });
 
 function handleCellClick(index) {
-    if (board[index] !== '') return; // Ignore already filled cells
+    if (board[index] !== '') return; 
 
     board[index] = currentPlayer === 1 ? 'X' : 'O';
     document.getElementById(index + 1).innerText = board[index];
@@ -36,9 +36,9 @@ function handleCellClick(index) {
 
 function checkWin() {
     const winPatterns = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
-        [0, 4, 8], [2, 4, 6]            // diagonals
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]            
     ];
 
     return winPatterns.some(pattern => {
